cfb36a8e377e20a08a9efa39feddfd7898ba0ae0
Hannah Gaskins <gaskinshannah@gmail.com>
2016-04-12 11:22:33 -0700

    bootstrap changes to columns, and randy recommended div order. also readded in images based on new container widths

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ var rawData = [
         title:          'Spot a Salmon',
         tagline:        'tagtest',
         description:    '<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula,                eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>',
-        // imgSRC:         'img/jekyll.png',
+        imgSRC:         'img/jekyll.png',
         category:       'website category',
         urlToSite:      'https://twitter.com/hannahgaskins',
         publishedOn:    '2015-10-18'
@@ -12,7 +12,7 @@ var rawData = [
         title:          'test title 2',
         tagline:        'tagtest 2',
         description:    '<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec id elit non mi porta gravida at eget metus. Donec sed odio dui. Curabitur blandit tempus porttitor.</p>',
-        // imgSRC:         'img/portland.png',
+        imgSRC:         'img/portland.png',
         category:       'website category 2',
         urlToSite:      'https://twitter.com/hannahgaskins',
         publishedOn:     '2016-03-18'
@@ -21,7 +21,7 @@ var rawData = [
         title:          'test title 3',
         tagline:        'tagtest 3',
         description:    '<p>Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.</p>',
-        // imgSRC:         'img/wine.png',
+        imgSRC:         'img/wine.png',
         category:       'website category 3',
         urlToSite:      'https://twitter.com/hannahgaskins',
         publishedOn:     '2016-03-20'
