ff7415d7a7ce8d63d07b13808ed167df6d4d8318
Hannah Gaskins <gaskinshannah@gmail.com>
2016-04-11 22:12:23 -0700

    added in dummy text for descriptions

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ var rawData = [
     {
         title:          'Spot a Salmon',
         tagline:        'tagtest',
-        description:    '<p>more testing</p>',
+        description:    '<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula,                eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>',
         // imgSRC:         'img/jekyll.png',
         category:       'website category',
         urlToSite:      'https://twitter.com/hannahgaskins',
@@ -11,7 +11,7 @@ var rawData = [
     {
         title:          'test title 2',
         tagline:        'tagtest 2',
-        description:    '<p>more testing 2</p>',
+        description:    '<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec id elit non mi porta gravida at eget metus. Donec sed odio dui. Curabitur blandit tempus porttitor.</p>',
         // imgSRC:         'img/portland.png',
         category:       'website category 2',
         urlToSite:      'https://twitter.com/hannahgaskins',
@@ -20,7 +20,7 @@ var rawData = [
     {
         title:          'test title 3',
         tagline:        'tagtest 3',
-        description:    '<p>more testing 3</p>',
+        description:    '<p>Aenean lacinia bibendum nulla sed consectetur. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.</p>',
         // imgSRC:         'img/wine.png',
         category:       'website category 3',
         urlToSite:      'https://twitter.com/hannahgaskins',
