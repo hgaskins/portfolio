16ca6cf46b8ef8bcc336301002b3b3b019a94f9a
Hannah Gaskins <gaskinshannah@gmail.com>
2016-04-13 15:28:53 -0700

    refactored delegation code for hide and show on index content

--- js/portfolioView.js
+++ js/portfolioView.js
@@ -1,19 +1,32 @@
-//view object to hold functions for dynamic updates
-var portfolioView = {};
+// //view object to hold functions for dynamic updates
+// var portfolioView = {};
+//
+// portfolioView.handleMainNav = function() {
+//     $('.main-nav').on('click', function() {
+//         $('li.portfolioLink').click(function() {
+//             $('#about').hide();
+//             $('#projects').show();
+//         });
+//         $('li.aboutLink').click(function() {
+//             $('#about').show();
+//             $('#projects').hide();
+//         });
+//     });
+// };
+//
+// $(document).ready(function() {
+//     portfolioView.handleMainNav();
+// });
 
-portfolioView.handleMainNav = function() {
-    $('.main-nav').on('click', function() {
-        $('li.portfolioLink').click(function() {
-            $('#about').hide();
-            $('#projects').show();
-        });
-        $('li.aboutLink').click(function() {
-            $('#about').show();
-            $('#projects').hide();
-        });
-    });
-};
+var $ul = $('ul.dropdown-menu');
 
-$(document).ready(function() {
-    portfolioView.handleMainNav();
-})
+$ul.on('click', 'li', function() {
+    if ($(this)[0].className == 'portfolioLink') {
+        $('#about').hide();
+        $('#projects').show();
+    } else if ($(this)[0].className == 'aboutLink') {
+        $('#about').show();
+        $('#projects').hide();
+    };
+    console.log($(this));
+});
